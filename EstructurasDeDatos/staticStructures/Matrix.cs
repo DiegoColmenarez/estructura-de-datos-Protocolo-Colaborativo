4d6e0ce685598cb24c6c2391e1758219cb206ae7
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-24 14:57:18 -0500

    refactor/modify parameter map in TransformaMatrix

--- EstructurasDeDatos/staticStructures/Matrix.cs
+++ EstructurasDeDatos/staticStructures/Matrix.cs
@@ -2,7 +2,7 @@ namespace EstructurasDeDatos.staticStructures;
 
 public class Matrix
 {
-    private static int[,] TransformMatrix(int[,] matrix, Func<int, int, int> map) {
+    private static int[,] TransformMatrix(int[,] matrix, Func<int, int, int, int> map) {
         var result  = new int[matrix.GetLength(0), matrix.GetLength(1)];
         for (var i = 0; i < matrix.GetLength(0); i++) {
             for (var j = 0; j < matrix.GetLength(1); j++) {
