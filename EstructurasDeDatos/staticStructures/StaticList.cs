a7e7191042e08c40798a371cb03f62565316ba7a
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-23 21:04:08 -0500

    feat/ creation and implementation of the PrintArray method

--- EstructurasDeDatos/staticStructures/StaticList.cs
+++ EstructurasDeDatos/staticStructures/StaticList.cs
@@ -2,9 +2,19 @@ namespace EstructurasDeDatos.staticStructures;
 
 public class StaticList
 {
-    private int[] array;
-    public StaticList(int[] array)
+    private int[] arrayNums;
+    public StaticList(int[] arrayNums)
     {
-        this.array = array;
+        this.arrayNums = arrayNums;
     }
+
+    public static void PrintArray(int[] arrayNums)
+    {
+        Console.WriteLine("Numeros en el arreglo:");
+        for (int i = 0; i < arrayNums.Length; i++)
+        {
+            Console.WriteLine("[indice: " + i + " Valor: " + arrayNums[i] + "]");
+        }
+    }
+    
 }
