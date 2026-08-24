631b97cfa1cb2949725ef6ea795aa5fe2db02d43
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-24 15:06:32 -0500

    refactor/ new implementation in TransformMatrix (parameter and vars)

--- EstructurasDeDatos/staticStructures/Matrix.cs
+++ EstructurasDeDatos/staticStructures/Matrix.cs
@@ -2,14 +2,15 @@ namespace EstructurasDeDatos.staticStructures;
 
 public class Matrix
 {
-    private static int[,] TransformMatrix(int[,] matrix, Func<int, int, int> map) {
+    private static int[,] TransformMatrix(int[,] matrix, Func<int, int,int, int> map) {
         var result  = new int[matrix.GetLength(0), matrix.GetLength(1)];
         for (var i = 0; i < matrix.GetLength(0); i++) {
             for (var j = 0; j < matrix.GetLength(1); j++) {
-                result[i, j] = map(matrix[i, j], i);
+                result[i, j] = map(matrix[i, j], i, j);
             }
         }
         return result;
     }
+   
     
 }
