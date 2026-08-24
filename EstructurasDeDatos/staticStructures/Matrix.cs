afd9c5ec68af7a00c59906c03b810862aeb58c99
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-24 14:52:12 -0500

    feat/ implementation of TransformMatrix method

--- EstructurasDeDatos/staticStructures/Matrix.cs
+++ EstructurasDeDatos/staticStructures/Matrix.cs
@@ -2,5 +2,13 @@ namespace EstructurasDeDatos.staticStructures;
 
 public class Matrix
 {
-    
+    private static int[,] TransformMatrix(int[,] matrix, Func<int, int, int> map) {
+        var result  = new int[matrix.GetLength(0), matrix.GetLength(1)];
+        for (var i = 0; i < matrix.GetLength(0); i++) {
+            for (var j = 0; j < matrix.GetLength(1); j++) {
+                result[i, j] = map(i, j);
+            }
+        }
+        return result;
+    }
 }
