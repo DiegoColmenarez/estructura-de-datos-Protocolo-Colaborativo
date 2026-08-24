069faaf181275be8ecbdbb939085ec1b74dbb2d7
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-24 17:18:54 -0500

    feat/ Implementation of TransFormList method

--- EstructurasDeDatos/dinamicStructures/DinamicList.cs
+++ EstructurasDeDatos/dinamicStructures/DinamicList.cs
@@ -2,5 +2,13 @@ namespace EstructurasDeDatos.dinamicStructures;
 
 public class DinamicList
 {
-    
+    private static List<int> TransformList(List<int> integerList, Func<int, int, int> operation)
+    {
+        var result = new List<int>(integerList.Count);
+        for (var i = 0; i < integerList.Count; i++)
+        {
+            result.Add(operation(integerList[i], i));
+        }
+        return result;
+    }
 }
