4a3eeece9536e701fc205f48ed538aaefb1204ee
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-26 14:02:14 -0500

    fix/ rename GetBestSelling method in IGetTotal interface

--- EstructurasDeDatos/exercise7/IGetTotal.cs
+++ EstructurasDeDatos/exercise7/IGetTotal.cs
@@ -2,5 +2,5 @@ namespace EstructurasDeDatos.exercise7;
 
 public interface IGetTotal
 {
-    List<Product> GetBestSelling(List<SalesLine> salesLineList);
+    List<Product> GetTotal(List<SalesLine> salesLineList);
 }
