be8054b2d85db79c439f7b994cfb6c8976829c2e
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-26 18:05:04 -0500

    Remove folder pythonVersion

--- EstructurasDeDatosPython/List.py
+++ EstructurasDeDatosPython/List.py
@@ -1,5 +1,3 @@
-list = (1, 2, 3, 4, 5, 6, 7, 9, 10)
-
 def transform_array(array_nums, operation):
         result_array = [0] * len(array_nums)
         for i in range(len(array_nums)):
@@ -8,8 +6,3 @@ def transform_array(array_nums, operation):
 
 def multiply_array_by_index(array_nums):
         return transform_array(array_nums, lambda value, index: index * value)
-
-
-list2 = multiply_array_by_index(list)
-print(list)
-print(list2)
